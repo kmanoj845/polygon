3f9fa9b0fc5614c7f1a8e9e8919dedbf9c0895dc
kmanoj845 <35392370+kmanoj845@users.noreply.github.com>
2024-10-10 10:24:50 +0530

    titanic code

--- main.py
+++ main.py
@@ -1,99 +1,90 @@
-import pandas as pd
-import time
-import yaml
-from pprint import pprint
-from datetime import datetime
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
-import ocr
-import image_correction
-import align_columns
-import helper as hp
-import merge_polygons
-import importlib
-importlib.reload(merge_polygons)
-importlib.reload(align_columns)
-importlib.reload(hp)
-importlib.reload(image_correction)
-importlib.reload(ocr)
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer, MinMaxScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-# Record the start time
-start_time = time.time()
+import seaborn as sns
 
-image_name = 'RS001-1977_1.png'
-year = '1977'
-format = 'form1'
-image_path = os.path.join(os.getcwd(), 'data', year, format, image_name)
+train_data = pd.read_csv(os.path.join(os.getcwd(), 'titanic', 'train.csv'))
+test_data = pd.read_csv(os.path.join(os.getcwd(), 'titanic', 'test.csv'))
 
-# read the cofig
-with open("config.yaml", 'r') as f:
-    config = yaml.safe_load(f)
+features = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
 
-# pprint(config)
-
-y_threshold = config[f'year_{year}'][format]['Y_THRESHOLD']
-proximity = config[f'year_{year}'][format]['PROXIMITY']
-merging_threshold = config[f'year_{year}'][format]['MERGING_THRESHOLD']
-school_pattern = config[f'year_{year}'][format]['SCHOOL_PATTERN']
-filtering_words = config[f'year_{year}'][format]['filtering_words']
-
-# BGR weights for image enhancement
-blue, green, red = [value for value in config[f'year_{year}'][format]['weights'].values()]
-
-if all(w == 0 for w in (blue, green, red)):
-    print('--- Calling OCR with original image ---')
-    ocr_data = ocr.get_ocr_data(image_path) # Call OCR API using original image
-    image_angle = round(ocr_data.to_dict()['pages'][0]['angle'], 2)
-else:
-    # Get image correction
-    print('--- Calling OCR with corrected image ---')
-    corrected_image = image_correction.do_image_correction(image_path, blue, green, red)
-    ocr_data = ocr.get_ocr_data(corrected_image)  # Call OCR API using corrected image
-    # corrected_image.show() # Uncomment this line to see corrected image
-
-ocr_words = ocr_data.to_dict()['pages'][0]['words']  # 'AnalyzeResult' object is not subscriptable, hence to_dict()
-
-def get_student_data(ocr_words):
-    # Sort the data first by y coordinate and then by x coordinate
-    sorted_data = sorted(ocr_words, key=lambda word: (word['polygon'][0]['y'], word['polygon'][0]['x']))
-
-    # Get rows based on how far Y values are
-    rows = hp.get_rows(sorted_data, Y_THRESHOLD=y_threshold)  # Threshold in pixels
-
-    # Sort each row by the top-left x-coordinate
-    sorted_rows = [hp.sort_by_top_left_x(row) for row in rows]
-
-    # Align the rows
-    aligned_rows = hp.align_rows_by_top_left_y(sorted_rows) # Make top y coordinates same 
-
-    # Merge close polygons (first end and next start x coordinate within merging_threshold pixels) 
-    merged_rows = [merge_polygons.merge_close_polygons(row, MERGING_THRESHOLD=merging_threshold) for row in aligned_rows]
-    # data_points = hp.cumulative_length(merged_rows)
-
-    exam_name = hp.extract_exam_name(merged_rows)
-    school_centre_list = hp.get_school_centre_code_name(merged_rows, school_pattern)
-    # student_data = hp.collect_student_rows(merged_rows, school_centre_list, filtering_words)
-    student_data = hp.collect_rows(merged_rows, school_centre_list, filtering_words)
-    return student_data
-
-student_data = get_student_data(ocr_words)
-all_words = [word for inner_list in student_data for word in inner_list]
-student_data = get_student_data(all_words)
-align_function_name = config[f'year_{year}'][format]['ALIGN_Fn']
-# create grid from student data
-grid = align_columns.arrange_data(student_data, proximity, align_function_name)
+train_df = train_data[features]
+train_df = train_df.dropna(subset=['Embarked'])
+train_df['IsAgeMissing'] = train_df['Age'].isna().astype(int)
+# y = train_df.pop('Survived')
 
 
-# Get the current date and time
-formatted_time = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-# # print("Formatted date and time:", formatted_time)
-save_file_name = image_path.split('.')[0]
-save_file_path = os.path.join((os.getcwd()),f'{save_file_name}_{formatted_time}.xlsx') 
-df = pd.DataFrame(grid)
-df.to_excel(save_file_path, index=False, header=False)
-print(f'---saved at {save_file_path}--')
-end_time = time.time()
+# Apply the default theme
+sns.set_theme()
+sns.catplot(data=train_df, kind="swarm", x="Survived", y="Age", hue="Sex")
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
-# Calculate the elapsed time
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.2f} seconds")
+plt.bar(train_df['Survived'], train_df['Age'])
+plt.show(block=True)
 
+# age_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='constant', fill_value=-1)),
+#     ('scaler', MinMaxScaler())])
+
+# def log_transform(x):
+#     return np.log1p(x+1.01)
+
+# fare_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='constant', fill_value=-1)),
+#     ('scaler', FunctionTransformer(log_transform))])
+
+# categorical_features = ['Embarked', 'Sex', 'Pclass']
+# for col in categorical_features:
+#     train_data[col] = train_data[col].astype('category')
+
+# categorical_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='most_frequent')),
+#     ('OneHotEncoder', OneHotEncoder(drop= 'first',handle_unknown='error'))])
+
+# preprocessor = ColumnTransformer(
+#     transformers=[
+#         ('age', age_transformer, ['Age']),
+#         ('fare', fare_transformer, ['Fare']),
+#         ('cat', categorical_transformer, categorical_features)],
+#         remainder='passthrough'
+# )
+
+
+# from sklearn.ensemble import RandomForestClassifier
+
+# RFmodel = RandomForestClassifier(n_estimators=100, max_depth=7, random_state=1)
+
+# clf = Pipeline([
+#     ('preprocessing', preprocessor),
+#     ('model', RFmodel)
+# ])
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     train_df, y, test_size=0.1, random_state=1)
+
+# # Fit the pipeline to the data
+# clf.fit(X_train, y_train)
+# print("model score: %.3f" % clf.score(X_train, y_train))
+# print("model score: %.3f" % clf.score(X_test, y_test))
+
+# clf.fit(train_df, y)
+# print("model score: %.3f" % clf.score(train_df, y))
+
+# features = [item for item in features if item != 'Survived']
+# test_df = test_data[features]
+# test_df['IsAgeMissing'] = test_df['Age'].isna().astype(int)
+
+# predictions = clf.predict(test_df)
+
+# output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+# save_path = os.path.join(os.getcwd(), 'titanic', 'submission.csv')
+# output.to_csv(save_path, index=False)
+# print("Your submission was successfully saved!")
---
+++ main_078229_score.py
@@ -0,0 +1,77 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer, MinMaxScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+train_data = pd.read_csv(os.path.join(os.getcwd(), 'titanic','train.csv'))
+test_data = pd.read_csv(os.path.join(os.getcwd(), 'titanic', 'test.csv'))
+
+features = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+
+train_df = train_data[features]
+train_df = train_df.dropna(subset=['Embarked'])
+train_df['IsAgeMissing'] = train_df['Age'].isna().astype(int)
+y = train_df.pop('Survived')
+
+age_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value=-1)),
+    ('scaler', MinMaxScaler())])
+
+def log_transform(x):
+    return np.log1p(x+1.01)
+
+fare_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value=-1)),
+    ('log transform', FunctionTransformer(log_transform))])
+
+categorical_features = ['Embarked', 'Sex', 'Pclass']
+for col in categorical_features:
+    train_data[col] = train_data[col].astype('category')
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('OneHotEncoder', OneHotEncoder(drop= 'first',handle_unknown='error'))])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('age', age_transformer, ['Age']),
+        ('fare', fare_transformer, ['Fare']),
+        ('cat', categorical_transformer, categorical_features)],
+        remainder='passthrough'
+)
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+RFmodel = RandomForestClassifier(n_estimators=100, max_depth=7, random_state=1)
+
+clf = Pipeline([
+    ('preprocessing', preprocessor),
+    ('model', RFmodel)
+])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    train_df, y, test_size=0.1, random_state=1)
+
+# Fit the pipeline to the data
+clf.fit(X_train, y_train)
+print("model score: %.3f" % clf.score(X_train, y_train))
+print("model score: %.3f" % clf.score(X_test, y_test))
+
+clf.fit(train_df, y)
+print("model score: %.3f" % clf.score(train_df, y))
+
+features = [item for item in features if item != 'Survived']
+test_df = test_data[features]
+test_df['IsAgeMissing'] = test_df['Age'].isna().astype(int)
+
+predictions = clf.predict(test_df)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+save_path = os.path.join(os.getcwd(), 'titanic', 'submission.csv')
+output.to_csv(save_path, index=False)
+print("Your submission was successfully saved!")
